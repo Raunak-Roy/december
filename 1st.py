4d081aaef16469bf66899429123c6de69428e931
email <raunakroy.india@gmail.com>
2023-12-24 16:36:52 +0530

    1st program in python

---
+++ 1st.py
@@ -0,0 +1,19 @@
+#printing hello world in pycharm
+
+print("Hello World")
+print('Hello God thanks for this kind opportunity')
+print('thanks alot dear for the kind support'
+      'always with you')
+
+a =4
+b =5
+print(a*b) # expecting multiplication
+print(a>b) # expecting true or false
+print(a%b)
+print(a//b)
+
+a=13
+if a>=18:
+    print("adults man")
+else:
+    print("boys")
